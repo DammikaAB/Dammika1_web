911e52b9223dbae190114c1e4b3ba3fad1ff916f
DammikaAB <dammika22000@student.hamk.fi>
2023-02-01 11:23:19 +0200

    Update exe1.php

--- layout.html/exe1.php
+++ layout.html/exe1.php
@@ -12,13 +12,14 @@ echo "BBCAP22";
 ?>
     <h1>3.2 Write PHP code to display the following message. </h1>
 <?php
-echo "Hello world! My name is \"Dammika\"";
+echo "Hello world! My name is \"Dammika A BAndara\"";
 ?>
     <h1>3.3 Write the PHP code in to display the current date.  </h1>
 
 <?php echo date("m.d.Y")?>
 <?php
- echo $title = "PHP is interesting” Put this variable as a title marked as h1 (heading 1) in your HTML document. 
+ echo $title = "PHP is interesting” Put this variable as a title marked as h1 (heading 1) in your HTML document. ?php>
+ 
 
 
 <?php include "footer.php"?>
