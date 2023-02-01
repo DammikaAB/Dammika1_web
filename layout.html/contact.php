cf061138f19acfeae0b286fff90de87d6d2c3dfd
DammikaAB <dammika22000@student.hamk.fi>
2023-02-01 10:54:55 +0200

    Update contact.php

--- layout.html/contact.php
+++ layout.html/contact.php
@@ -1,6 +1,4 @@
-<?php 
-$title = "Contact; HAMK oy";
-include "header.php"?>
+<?php include "header.php"?>
 
 <h2>About us ...</h2>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium eum ratione blanditiis? Quia voluptate tempora veritatis maxime nostrum accusantium pariatur minus odit suscipit voluptatem exercitationem, repellat cum aliquid quas magni?</p>
