1163af2b299f13e78c1f2e708397057c62f75cfa
DammikaAB <dammika22000@student.hamk.fi>
2023-02-01 10:29:36 +0200

    Initial commit

---
+++ layout.html/contact.php
@@ -0,0 +1,8 @@
+<?php 
+$title = "Contact; HAMK oy";
+include "header.php"?>
+
+<h2>About us ...</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium eum ratione blanditiis? Quia voluptate tempora veritatis maxime nostrum accusantium pariatur minus odit suscipit voluptatem exercitationem, repellat cum aliquid quas magni?</p>
+
+<?php include "footer.php"?>
