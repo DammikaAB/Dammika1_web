561d88d8ed253bc3fbe38ca50dd8ce3a0f380de0
DammikaAB <dammika22000@student.hamk.fi>
2023-02-01 11:08:52 +0200

    Changed Exe1.php Footer
    Changed Exe1.php Footer

--- layout.html/exe1.php
+++ layout.html/exe1.php
@@ -1,7 +1,11 @@
-<?php include "header.php"?>
+<?php 
+echo $title = "PHP is interesting”
+
+include "header.php"?>
 
 <body>
     <h1>3.1 Write a simple PHP script to print your information (Name and your groupid). </h1>
+    
 <?php
 echo "Dammika A Bandara";
 echo "BBCAP22";
@@ -11,6 +15,10 @@ echo "BBCAP22";
 echo "Hello world! My name is \"Dammika\"";
 ?>
     <h1>3.3 Write the PHP code in to display the current date.  </h1>
+
 <?php echo date("m.d.Y")?>
+<?php
+ $title = "PHP is interesting” Put this variable as a title marked as h1 (heading 1) in your HTML document. 
+
 
 <?php include "footer.php"?>
