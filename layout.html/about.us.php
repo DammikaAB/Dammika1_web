634449c67d6fb0bdfbd2fdb824563a8dd92c85ea
DammikaAB <dammika22000@student.hamk.fi>
2023-02-01 10:53:51 +0200

    Dammika1.web
    Dammika1.Web

--- layout.html/about.us.php
+++ layout.html/about.us.php
@@ -1,8 +1,6 @@
-<?php
-$title = "About us; HAMK oy";
-include "header.php"?>
+<?php include "header.php"?>
 
 <h2>About us ...</h2>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium eum ratione blanditiis? Quia voluptate tempora veritatis maxime nostrum accusantium pariatur minus odit suscipit voluptatem exercitationem, repellat cum aliquid quas magni?</p>
-
+<p>Dammika A Bandara</p>
 <?php include "footer.php"?>
